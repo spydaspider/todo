d538c7050d04fb414b6e445a3d523f35359f96f8
spydaspider <spydaspider@gmail.com>
2023-05-29 08:47:54 +0000

    Add server

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const users = require('./routes/users.js');
+const todos = require('./routes/todos.js');
+require('dotenv').config();
+const app = express();
+app.use(express.json());
+app.use((req,res,next)=>{
+    console.log(req.path, req.body);
+    next();
+})
+const PORT = process.env.PORT || 5000;
+/* app.use('/api/users/',users);
+app.use('/api/routes/todos',todos); */
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`Connected to mongodb on port ${PORT}`);
+    })
+}).catch((error)=>{
+    console.error(error.message);
+})
