8836d7239e85d2c4598f0f61ef8cb832e4398d73
spydaspider <spydaspider@gmail.com>
2023-06-05 02:14:35 +0000

    Add logout

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React,{ Fragment } from 'react';
+import { Link,Redirect } from 'react-router-dom';
+import { useAuthContext } from '../hooks/useAuthContext';
+import { useLogout } from '../hooks/useLogout';
 const Navbar = () =>{
+    const { logout } = useLogout();
+    const handleLogout = () =>{
+        logout();
+    }
+    const { user } = useAuthContext();
     return(
         <div className = "nav-container">
              <div className = "todo-header">
                 <Link to = '/'>My Todo List</Link>
              </div>
              <nav>
-                <Link to = '/signup'>signup</Link> 
-                <Link to = '/login'>Login</Link>   
+                 
+                {user ? <button onClick = {handleLogout}>logout</button> : <Fragment><Link to = '/signup'>signup</Link>
+                <Link to = '/login'>Login</Link></Fragment>  }
+                
+               
+                
              </nav>
         </div>
         
